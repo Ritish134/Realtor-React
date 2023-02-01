80a3f56a28b075db28c72eb4eeff36f71052477c
Ritish134 <ritishnew@gmail.com>
2023-02-01 22:33:25 +0530

    added delete functionality to my listing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { collection, doc, getDoc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDoc, getDocs, orderBy, query, updateDoc, where } from 'firebase/firestore';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -7,6 +7,7 @@ import { db } from '../firebase';
 import {FcHome} from "react-icons/fc"
 import { useEffect } from 'react';
 import ListingItem from "../components/ListingItem";
+import { async } from '@firebase/util';
 
 export default function Profile() {
   const auth = getAuth()
@@ -68,6 +69,19 @@ export default function Profile() {
     }
     fetchUserListings();
   },[auth.currentUser.uid])
+  async function onDelete(listingID){
+    if(window.confirm("Are you sure you want to delete?")){
+      await deleteDoc(doc(db,"listings", listingID))
+      const updatedListings = listings.filter(
+        (listing)=> listing.id !== listingID
+      )
+      setListings(updatedListings)
+      toast.success("Successfully deleted the listing")
+    }
+  }
+  function onEdit(listingID){
+    navigate(`/edit-listing/${listingID}`)
+  }
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -109,6 +123,8 @@ export default function Profile() {
               <ListingItem key={listing.id} 
               id = {listing.id}
               listing = {listing.data}
+              onDelete={()=> onDelete(listing.id)}
+              onEdit={()=> onEdit(listing.id)}
               />
             ))}
            </ul>
